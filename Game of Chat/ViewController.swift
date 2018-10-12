7abd521b2cd6f119a85e7b2cd3160c927707cd80
Felix <linfelix165@gmail.com>
2018-10-12 00:22:49 -0400

    Initial Commit

---
+++ Game of Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game of Chat
+//
+//  Created by Felix Lin on 10/12/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
